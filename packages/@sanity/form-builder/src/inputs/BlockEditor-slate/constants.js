ca919ca82b66118ec72e2464863676afeb7f06ce
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2017-09-10 03:23:37 +0200

    [wip] Add default block constant

--- packages/@sanity/form-builder/src/inputs/BlockEditor-slate/constants.js
+++ packages/@sanity/form-builder/src/inputs/BlockEditor-slate/constants.js
@@ -1,2 +1,9 @@
 export const SLATE_DEFAULT_STYLE = 'normal'
 export const SLATE_SPAN_TYPE = 'span'
+export const DEFAULT_BLOCK_TYPE = {
+  type: 'contentBlock',
+  kind: 'block',
+  data: {
+    style: SLATE_DEFAULT_STYLE
+  }
+}
