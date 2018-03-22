7b7700338285f35b17d56db6d9beeb093e0d1273
Espen Hovlandsdal <espen@hovlandsdal.com>
2018-03-22 15:04:31 +0100

    [form-builder] Psuedo-code: Hash files and check for presence before upload

--- packages/@sanity/form-builder/src/sanity/inputs/client-adapters/assets.js
+++ packages/@sanity/form-builder/src/sanity/inputs/client-adapters/assets.js
@@ -4,7 +4,15 @@ import {withMaxConcurrency} from '../../utils/withMaxConcurrency'
 
 const MAX_CONCURRENT_UPLOADS = 4
 
-function uploadSanityAsset(assetType, file) {
+async function uploadSanityAsset(assetType, file) {
+  const documentType = `sanity.${assetType}Asset`
+  const hash = await hashFile(file)
+  if (hash) {
+    client.observable
+      .fetch('*[_type == $documentType && sha1sum == $hash][0]', {documentType, hash})
+      .subscribe() // If we get a result back, map to same shape as below, otherwise do upload
+  }
+
   return client.observable.assets.upload(assetType, file).map(event => {
     return event.type === 'response'
       ? {
@@ -24,4 +32,23 @@ export const uploadFileAsset = file => uploadAsset('file', file)
 
 export function materializeReference(id) {
   return observePaths(id, ['originalFilename', 'url', 'metadata'])
+}
+
+function hashFile(file) {
+  if (!window.crypto || !window.crypto.subtle || !window.FileReader) {
+    return false
+  }
+
+  const reader = new FileReader()
+  reader.onload = () =>
+    crypto.subtle.digest('SHA-1', reader.result).then(hash => {
+      hexFromBuffer(hash) // Resolve with this
+    })
+  reader.readAsArrayBuffer(file)
+}
+
+function hexFromBuffer(buffer) {
+  return Array.prototype.map
+    .call(new Uint8Array(buffer), x => `00${x.toString(16)}`.slice(-2))
+    .join('')
 }
